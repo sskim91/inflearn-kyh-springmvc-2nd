a065d9dc16e7c8cd8f7205653689ddc574a3342f
sskim91 <tjdtjq91@gmail.com>
2021-08-21 22:14:27 +0900

    타임리프 텍스트 - text, utext

---
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,27 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sskim on 2021/08/21
+ * Github : http://github.com/sskim91
+ */
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+}
