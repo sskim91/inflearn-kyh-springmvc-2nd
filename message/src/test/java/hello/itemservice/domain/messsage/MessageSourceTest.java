d20e7a01810c736bca33097be17b61a6e40af182
sskim91 <tjdtjq91@gmail.com>
2021-10-11 18:04:01 +0900

    메시지, 국제화

---
+++ message/src/test/java/hello/itemservice/domain/messsage/MessageSourceTest.java
@@ -0,0 +1,59 @@
+package hello.itemservice.domain.messsage;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Created by sskim on 2021/10/11
+ * Github : http://github.com/sskim91
+ */
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Test
+    void helloMessage() throws Exception {
+        String result = messageSource.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() throws Exception {
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void notFoundMessageCodeDefaultMessage() throws Exception {
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() throws Exception{
+        String message = messageSource.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() throws Exception{
+        assertThat(messageSource.getMessage("hello",null,null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello",null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() throws Exception{
+        assertThat(messageSource.getMessage("hello",null, Locale.ENGLISH)).isEqualTo("hello");
+    }
+
+}
