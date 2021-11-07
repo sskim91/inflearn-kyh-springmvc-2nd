928932fc172237a8a3c50983b148012863ed14d9
sskim91 <tjdtjq91@gmail.com>
2021-11-08 00:14:09 +0900

    로그인처리 세션

---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,9 @@
+package hello.login.web;
+
+/**
+ * Created by sskim on 2021/11/07
+ * Github : http://github.com/sskim91
+ */
+public abstract class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
