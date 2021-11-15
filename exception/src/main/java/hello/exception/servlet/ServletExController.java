8a8378764a9c2e20cf03a1013126dbac6f635df5
sskim91 <tjdtjq91@gmail.com>
2021-11-16 00:09:28 +0900

    서블릿 예외 처리 - 시작 - 오류화면 제공 - 오류 페이지 작동 원리

---
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,34 @@
+package hello.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sskim on 2021/11/15
+ * Github : http://github.com/sskim91
+ */
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생!");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
