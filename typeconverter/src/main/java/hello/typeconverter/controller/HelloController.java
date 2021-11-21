d4289a25a0d51c9981530a0280104604b5b98fbd
sskim91 <tjdtjq91@gmail.com>
2021-11-21 16:13:16 +0900

    스프링 타입 컨버터 Init

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,32 @@
+package hello.typeconverter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by sskim on 2021/11/21
+ * Github : http://github.com/sskim91
+ */
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입 조회
+
+        Integer intValue = Integer.valueOf(data);   //숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
